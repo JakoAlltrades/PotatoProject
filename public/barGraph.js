1b34ca107d70d31840b2af8a8ad2574f0ea7077c
MBalderasNu <mexico2067@gmail.com>
2017-08-19 15:35:50 -0600

    bar graph
    heaven sent by GOD AKA MATTHEW

--- public/barGraph.js
+++ public/barGraph.js
@@ -1,188 +1,129 @@
-var cxt = document.getElementById("canvasId").getContext("2d");
-                        
-
-function BarGraph(ctx) {
-
-  // Private properties and methods
-	
-  var that = this;
-  var startArr;
-  var endArr;
-  var looping = false;
-		
-  // Loop method adjusts the height of bar and redraws if neccessary
-	var loop = function () {
-
-	  var delta;
-	  var animationComplete = true;
-
-	  // Boolean to prevent update function from looping if already looping
-	  looping = true;
-	  
-	  // For each bar
-	  for (var i = 0; i < endArr.length; i += 1) {
-		// Change the current bar height toward its target height
-		delta = (endArr[i] - startArr[i]) / that.animationSteps;
-		that.curArr[i] += delta;
-		// If any change is made then flip a switch
-		if (delta) {
-		  animationComplete = false;
-		}
-	  }
-	  // If no change was made to any bars then we are done
-	  if (animationComplete) {
-		looping = false;
-	  } else {
-		// Draw and call loop again
-		draw(that.curArr);
-		setTimeout(loop, that.animationInterval / that.animationSteps);
-	  }
-	};
-		
-  // Draw method updates the canvas with the current display
-	var draw = function (arr) {
-							
-	  var numOfBars = arr.length;
-	  var barWidth;
-	  var barHeight;
-	  var border = 2;
-	  var ratio;
-	  var maxBarHeight;
-	  var gradient;
-	  var largestValue;
-	  var graphAreaX = 0;
-	  var graphAreaY = 0;
-	  var graphAreaWidth = that.width;
-	  var graphAreaHeight = that.height;
-	  var i;
-	  
-		// Update the dimensions of the canvas only if they have changed
-	  if (ctx.canvas.width !== that.width || ctx.canvas.height !== that.height) {
-		ctx.canvas.width = that.width;
-		ctx.canvas.height = that.height;
-	  }
-				
-	  // Draw the background color
-	  ctx.fillStyle = that.backgroundColor;
-	  ctx.fillRect(0, 0, that.width, that.height);
-					
-	  // If x axis labels exist then make room	
-	  if (that.xAxisLabelArr.length) {
-		graphAreaHeight -= 40;
-	  }
-				
-	  // Calculate dimensions of the bar
-	  barWidth = graphAreaWidth / numOfBars - that.margin * 2;
-	  maxBarHeight = graphAreaHeight - 25;
-				
-	  // Determine the largest value in the bar array
-	  var largestValue = 0;
-	  for (i = 0; i < arr.length; i += 1) {
-		if (arr[i] > largestValue) {
-		  largestValue = arr[i];	
-		}
-	  }
-	  
-	  // For each bar
-	  for (i = 0; i < arr.length; i += 1) {
-		// Set the ratio of current bar compared to the maximum
-		if (that.maxValue) {
-		  ratio = arr[i] / that.maxValue;
-		} else {
-		  ratio = arr[i] / largestValue;
-		}
-		
-		barHeight = ratio * maxBarHeight;
-	  
-		// Turn on shadow
-		ctx.shadowOffsetX = 2;
-		ctx.shadowOffsetY = 2;
-		ctx.shadowBlur = 2;
-		ctx.shadowColor = "#999";
-						
-		// Draw bar background
-		ctx.fillStyle = "#333";			
-		ctx.fillRect(that.margin + i * that.width / numOfBars,
-		  graphAreaHeight - barHeight,
-		  barWidth,
-		  barHeight);
-			
-		// Turn off shadow
-		ctx.shadowOffsetX = 0;
-		ctx.shadowOffsetY = 0;
-		ctx.shadowBlur = 0;
-
-		// Draw bar color if it is large enough to be visible
-		if (barHeight > border * 2) {
-			// Create gradient
-			gradient = ctx.createLinearGradient(0, 0, 0, graphAreaHeight);
-			gradient.addColorStop(1-ratio, that.colors[i % that.colors.length]);
-			gradient.addColorStop(1, "#ffffff");
-
-			ctx.fillStyle = gradient;
-			// Fill rectangle with gradient
-			ctx.fillRect(that.margin + i * that.width / numOfBars + border,
-			  graphAreaHeight - barHeight + border,
-			  barWidth - border * 2,
-			  barHeight - border * 2);
-		}
-
-		// Write bar value
-		ctx.fillStyle = "#333";
-		ctx.font = "bold 12px sans-serif";
-		ctx.textAlign = "center";
-		// Use try / catch to stop IE 8 from going to error town
-		try {
-		  ctx.fillText(parseInt(arr[i],10),
-			i * that.width / numOfBars + (that.width / numOfBars) / 2,
-			graphAreaHeight - barHeight - 10);
-		} catch (ex) {}
-		// Draw bar label if it exists
-		if (that.xAxisLabelArr[i]) {					
-		  // Use try / catch to stop IE 8 from going to error town				
-		  ctx.fillStyle = "#333";
-		  ctx.font = "bold 12px sans-serif";
-		  ctx.textAlign = "center";
-		  try{
-			ctx.fillText(that.xAxisLabelArr[i],
-			  i * that.width / numOfBars + (that.width / numOfBars) / 2,
-			  that.height - 10);
-			} catch (ex) {}
-		  }
-		}
-	  };
-
-  // Public properties and methods
-	
-  this.width = 300;
-  this.height = 150;	
-  this.maxValue;
-  this.margin = 5;
-  this.colors = ["purple", "red", "green", "yellow"];
-  this.curArr = [];
-  this.backgroundColor = "#fff";
-  this.xAxisLabelArr = [];
-  this.yAxisLabelArr = [];
-  this.animationInterval = 100;
-  this.animationSteps = 10;
-	
-  // Update method sets the end bar array and starts the animation
-	this.update = function (newArr) {
-
-	  // If length of target and current array is different 
-	  if (that.curArr.length !== newArr.length) {
-		that.curArr = newArr;
-		draw(newArr);
-	  } else {
-		// Set the starting array to the current array
-		startArr = that.curArr;
-		// Set the target array to the new array
-		endArr = newArr;
-		// Animate from the start array to the end array
-		if (!looping) {	
-		  loop();
-		}
-	  }
-	}; 
+var myCanvas = document.getElementById("myCanvas");
+myCanvas.width = 300;
+myCanvas.height = 300;
+   
+var ctx = myCanvas.getContext("2d");
+ 
+function drawLine(ctx, startX, startY, endX, endY,color){
+    ctx.save();
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(endX,endY);
+    ctx.stroke();
+    ctx.restore();
 }
-BarGraph("canvasId");
+ 
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height,color){
+    ctx.save();
+    ctx.fillStyle=color;
+    ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+    ctx.restore();
+}
+ 
+var myVinyls = {
+    "Classical music": 10,
+    "Alternative rock": 14,
+    "Pop": 2,
+    "Jazz": 12
+};
+ 
+var Barchart = function(options){
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+  
+    this.draw = function(){
+        var maxValue = 0;
+        for (var categ in this.options.data){
+            maxValue = Math.max(maxValue,this.options.data[categ]);
+        }
+        var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+        var canvasActualWidth = this.canvas.width - this.options.padding * 2;
+ 
+        //drawing the grid lines
+        var gridValue = 0;
+        while (gridValue <= maxValue){
+            var gridY = canvasActualHeight * (1 - gridValue/maxValue) + this.options.padding;
+            drawLine(
+                this.ctx,
+                0,
+                gridY,
+                this.canvas.width,
+                gridY,
+                this.options.gridColor
+            );
+             
+            //writing grid markers
+            this.ctx.save();
+            this.ctx.fillStyle = this.options.gridColor;
+            this.ctx.textBaseline="bottom"; 
+            this.ctx.font = "bold 10px Arial";
+            this.ctx.fillText(gridValue, 10,gridY - 2);
+            this.ctx.restore();
+ 
+            gridValue+=this.options.gridScale;
+        }      
+  
+        //drawing the bars
+        var barIndex = 0;
+        var numberOfBars = Object.keys(this.options.data).length;
+        var barSize = (canvasActualWidth)/numberOfBars;
+ 
+        for (categ in this.options.data){
+            var val = this.options.data[categ];
+            var barHeight = Math.round( canvasActualHeight * val/maxValue) ;
+            drawBar(
+                this.ctx,
+                this.options.padding + barIndex * barSize,
+                this.canvas.height - barHeight - this.options.padding,
+                barSize,
+                barHeight,
+                this.colors[barIndex%this.colors.length]
+            );
+ 
+            barIndex++;
+        }
+ 
+        //drawing series name
+        this.ctx.save();
+        this.ctx.textBaseline="bottom";
+        this.ctx.textAlign="center";
+        this.ctx.fillStyle = "#000000";
+        this.ctx.font = "bold 14px Arial";
+        this.ctx.fillText(this.options.seriesName, this.canvas.width/2,this.canvas.height);
+        this.ctx.restore();  
+         
+        //draw legend
+        barIndex = 0;
+        var legend = document.querySelector("legend[for='myCanvas']");
+        var ul = document.createElement("ul");
+        legend.append(ul);
+        for (categ in this.options.data){
+            var li = document.createElement("li");
+            li.style.listStyle = "none";
+            li.style.borderLeft = "20px solid "+this.colors[barIndex%this.colors.length];
+            li.style.padding = "5px";
+            li.textContent = categ;
+            ul.append(li);
+            barIndex++;
+        }
+    }
+}
+ 
+ 
+var myBarchart = new Barchart(
+    {
+        canvas:myCanvas,
+        seriesName:"Vinyl records",
+        padding:20,
+        gridScale:5,
+        gridColor:"#eeeeee",
+        data:myVinyls,
+        colors:["#a55ca5","#67b6c7", "#bccd7a","#eb9743"]
+    }
+
+);
+
+myBarchart.draw();
