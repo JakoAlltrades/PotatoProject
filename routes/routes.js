322c83359898fe9f9d6d0c0453334ee7d55e6145
JakoAlltrades <Jpriemo@gmail.com>
2017-08-20 12:24:19 -0600

    Fixed styling and got count to increment
    but the return does not work, it is still returning at the start of the
    call, will keep working on but might need help

--- routes/routes.js
+++ routes/routes.js
@@ -179,7 +179,76 @@ exports.delete = function (req, res) {
 
 exports.details = function(req, res)
 {
-    User.find(function(err, user){
+    var done = false;
+    var boiledNum, bakedNum, MashedNum, FriedNum, sweetNum, russetNum, redNum, whiteNum, friesNum, totsNum, wedgeNum, hashNum;
+    for(var j = 0; j < 12; j++)
+        {
+            if(j==0)
+                {
+                     boiledNum = countAnswerOne('Boiled');
+                }
+            else if(j == 1)
+                {
+                    bakedNum = countAnswerOne('Baked');
+                }
+            else if(j == 2)
+                {
+                    MashedNum = countAnswerOne('Mashed');
+                }
+            else if(j == 3)
+                {
+                   friedNum = countAnswerOne('Fried');
+                }
+            else if(j==4)
+                {
+                     sweetNum = countAnswerTwo("Sweet potatoes");
+                }
+            else if(j == 5)
+                {
+                    russetNum = countAnswerTwo("Russet potatoes");
+                }
+            else if(j == 6)
+                {
+                    redNum = countAnswerTwo("Red potatoes");
+                }
+            else if(j == 7)
+                {
+                   whiteNum = countAnswerTwo("White potatoes");
+                }
+            else if(j==8)
+                {
+                     friesNum = countAnswerThree("French Fries");
+                }
+            else if(j == 9)
+                {
+                    totsNum = countAnswerThree("Tatter tots");
+                }
+            else if(j == 10)
+                {
+                    wedgeNum = countAnswerThree("Potato Wedges");
+                }
+            else if(j == 11)
+                {
+                   hashNum = countAnswerThree("Hash Browns");
+                    done = true;
+                }
+            
+                }
+    if(done == true)
+                {
+                    console.log("boiledNum: ", boiledNum);
+                    console.log("bakedNum: ", bakedNum);
+                    console.log("mashedNum: ", MashedNum);
+                    console.log("FriedNum: ", FriedNum);
+                    console.log("sweetNum: ", sweetNum);
+                    console.log("russetNum: ", russetNum);
+                    console.log("redNum: ", redNum);
+                    console.log("whiteNum: ", whiteNum);
+                    console.log("friesNum: ", friesNum);
+                    console.log("totsNum: ", totsNum);
+                    console.log("wedgeNum: ", wedgeNum);
+                    console.log("hashNum: ", hashNum);
+                    User.find(function(err, user){
         user.userName = curUser.userName;
         user.password = curUser.password;
         user.email = curUser.email;
@@ -193,75 +262,140 @@ exports.details = function(req, res)
         res.render('details', {
             title:  "Details",
             user: user,
-            boiledNum: countAnswerOne('Boiled'),
-            bakedNum: countAnswerOne('Baked'),
-            MashedNum: countAnswerOne('Mashed'),
-            friedNum: countAnswerOne('Fried'),
-            sweetNum: countAnswerTwo("Sweet potatoes"),
-            russetNum: countAnswerTwo("Russet potatoes"),
-            redNum: countAnswerTwo("Red potatoes"),
-            whiteNum: countAnswerTwo("White potatoes"),
-            friesNum: countAnswerThree("French Fries"),
-            totsNum: countAnswerThree("Tatter tots"),
-            wedgeNum: countAnswerThree("Potato Wedges"),
-            hashNum: countAnswerThree("Hash Browns")
+            boiledNum: boiledNum,
+            bakedNum: bakedNum,
+            MashedNum: MashedNum,
+            FriedNum: FriedNum,
+            sweetNum: sweetNum,
+            russetNum: russetNum,
+            redNum: redNum,
+            whiteNum: whiteNum,
+            friesNum: friesNum,
+            totsNum: totsNum,
+            wedgeNum: wedgeNum,
+            hashNum: hashNum
         });
         
 });
+        }
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 
 function countAnswerOne(collection)
 {
+    var done = false;
+     var tempUser = new User({
+        userAnswer1: collection
+    });
     var count = 0;
-    console.log("word: ", collection );
     User.find(function (err, user) {
     if (err) return console.error(err);
         for(var j = 0; j < user.length; j++)
             {
-                console.log("Answer1: ",user[j].userAnswer1);
-                if(user[j].userAnswer1.prototype.toLowerCase == collection.toLowerCase)
+                if(j == 0)
                     {
+                        console.log("word: ", tempUser.userAnswer1);
+                    }
+                var UA1 = user[j].userAnswer1;
+                console.log("UA1: ",UA1);
+                if(UA1 == tempUser.userAnswer1)
+                    {
+                        console.log("Match!");
                        count++; 
+                        console.log("curCount: ", count);
+                    }
+                if(j == user.length - 1)
+                    {
+                        done = true;
+                    }
+                if(done == true)
+                    {
+                        console.log("return count for " + collection + ":", count);
+                        return count;
                     }
             }
     });
-    return count;
 }
 
 function countAnswerTwo(collection)
 {
+    var done = false;
+    var tempUser = new User({
+        userAnswer2: collection
+    });
     var count = 0;
-    console.log("word: ", collection );
     User.find(function (err, user) {
     if (err) return console.error(err);
         for(var j = 0; j < user.length; j++)
             {
-                console.log("Answer2: ",user[j].userAnswer2);
-                if(user[j].userAnswer2.prototype.toLowerCase == collection.toLowerCase)
+                if(j == 0)
                     {
+                        console.log("word: ", tempUser.userAnswer2);
+                    }
+                var UA2 = user[j].userAnswer2;
+                console.log("UA2: ",UA2);
+                if(UA2 == tempUser.userAnswer2)
+                    {
+                        console.log("Match!");
                        count++; 
+                        console.log("curCount: ", count);
+                    }
+                if(j == user.length - 1)
+                    {
+                        done = true;
+                    }
+                if(done == true)
+                    {
+                        console.log("return count for " + collection + ":", count);
+                        return count;
                     }
             }
     });
-    return count;
 }
 
 function countAnswerThree(collection)
 {
+    var done = false;
+     var tempUser = new User({
+        userAnswer3: collection
+    });
     var count = 0;
-    console.log("word: ", collection );
     User.find(function (err, user) {
     if (err) return console.error(err);
-        console.log("user length", user.length);
         for(var j = 0; j < user.length; j++)
             {
-                console.log("Answer3: ",user[j].userAnswer3);
-                if(user[j].userAnswer3.prototype.toLowerCase == collection.toLowerCase)
+                if(j == 0)
                     {
+                        console.log("word: ", tempUser.userAnswer3);
+                    }
+                var UA3 = user[j].userAnswer3;
+                console.log("UA3: ",UA3);
+                if(UA3 == tempUser.userAnswer3)
+                    {
+                        console.log("Match!");
                        count++; 
+                        console.log("curCount: ", count);
+                    }
+                if(j == user.length - 1)
+                    {
+                        done = true;
+                    }
+                if(done == true)
+                    {
+                        console.log("return count for " + collection + ":", count);
+                        return count;
                     }
             }
     });
-    return count;
+    
 }
